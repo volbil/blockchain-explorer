e38ccd748679a05f9f7c13cd2e7673896a525799
Volodymyr Biloshytskyi <volbilnexus@gmail.com>
2018-09-03 13:15:25 +0900

    Added support of POST request and some code improvements

--- api/index.php
+++ api/index.php
@@ -2,34 +2,41 @@
 	header("Access-Control-Allow-Origin: *");
 	header('Content-Type: application/json');
 
+	$timeout = 10; // seconds
 	$rpc_url = "";
-	$error = '{"jsonrpc": "2.0", "error": {"code": -32001, "message": "Server not responding :("}, "id": null}';
+	$error = [
+		"not-responding" => '{"jsonrpc": "2.0", "error": {"code": -32001, "message": "Server Not Responding"}, "id": null}',
+		"invalid-request" => '{"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null}'
+	];
 
-	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	if (in_array($_SERVER["REQUEST_METHOD"], ["GET", "POST"])) {
+		$request = isset($_GET) ? $_GET : $_POST;
+
 		$params = json_encode([
-			'jsonrpc' 	=> "2.0",
-			'method' 	=> isset($_GET["method"]) ? $_GET["method"] : "",
-			'params' 	=> isset($_GET["params"]) ? $_GET["params"] : [],
-			'id' 		=> "explorer"
+			"jsonrpc" 	=> "2.0",
+			"method" 	=> isset($request["method"]) ? $request["method"] : "",
+			"params" 	=> isset($request["params"]) ? $request["params"] : [],
+			"id" 		=> "explorer"
 		]);
 
 		$content = @file_get_contents($rpc_url, null, stream_context_create(array(
-			'http' => array(
-				'method' => 'POST',
-				'header' => 'Content-Type: application/json' . "\r\n"
-				. 'Content-Length: ' . strlen($params) . "\r\n",
-				'content' => $params,
-				'timeout' => 10, // 10 seconds
+			"http" => array(
+				"method" => "POST",
+				"header" => "Content-Type: application/json" . "\r\n"
+				. "Content-Length: " . strlen($params) . "\r\n",
+				"content" => $params,
+				"timeout" => $timeout,
 			),
 		)));
 
 		if($content === false) {
-			$result = $error;
+			$result = $error["not-responding"];
 		} else {
 			$result = $content;
 		}
 
+		echo $result;
+	} else {
+		echo $error["invalid-request"];
 	}
-
-	echo $result;
 ?>
